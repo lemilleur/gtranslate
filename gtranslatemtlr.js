80c9797ccbabffdd25c79b834826c169dce60750
lemilleur <49449952+lemilleur@users.noreply.github.com>
2025-03-15 16:02:02 +0300

    Update gtranslatemtlr.js

--- gtranslatemtlr.js
+++ gtranslatemtlr.js
@@ -14,7 +14,7 @@ const googleTranslateConfig = {
     /* Если скрипт не работает на поддомене, 
     раскомментируйте и
     укажите основной домен в свойстве domain */
-    domain: "webcreww.ru"
+    domain: "matller.com"
 };
 
 function TranslateInit() {
